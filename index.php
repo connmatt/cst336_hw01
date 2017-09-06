cf2747804ce0a8311b5779778f03d409bf500dda
Matthew Connolly <31319766+connmatt@users.noreply.github.com>
2017-09-06 11:30:54 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
 <?php include_once("GameDev/home.html"); ?>
-<?php include_once("GameDev/unity.html"); ?>
-<?php include_once("GameDev/csharp.html"); ?>
-<?php include_once("GameDev/sprites.html"); ?>
